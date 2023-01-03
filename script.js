50189c7ac519ce5d85c8ba859ea28c220408b259
Adams Fontil <adamsfuntil@gmail.com>
2023-01-02 23:28:27 -0500

    create basic playerFactory and basic gameboard module"

--- script.js
+++ script.js
@@ -1,29 +1,33 @@
+
 //gameboard module
 const gameBoard = (() => {
-    board = [1,2,3,4,5,6,7,8,9]
-    let testing = () => console.log('testing')
-    return {
-     testing,
-     board
-    };
-  })();
-
-
-
-//players
-const player = ( (name, marker) => {
-  name,
-  marker,
-  sayName = function() {
-    console.log(name)
-  }
+  board = [1,2,3,4,5,6,7,8,9]
+  let testing = () => console.log('testing')
   return {
-    name,
-    sayName
+   testing,
+   board
   };
-}) ();
+})();
 
-const tic = document.querySelector('tic-tac-toe');
+
+
+//players factory
+const playerFactory = (name, marker) => {
+const sayHello = () => console.log('hello!');
+return { name, marker, sayHello };
+};
+
+const jeff = playerFactory('jeff', 'X');
+
+console.log(jeff.name); // 'jeff'
+
+jeff.sayHello(); // calls the function and logs 'hello!'
+
+
+
+
+
+//make the game work
 
 
 
