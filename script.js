9e1344fa2253af29a3fc5814be231b4b58560b40
Adams Fontil <adamsfuntil@gmail.com>
2023-01-04 16:49:05 -0500

    basic game finished but it needs works

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 hideOverlay()
+hideOverlay2()
 //gameboard module
 const gameBoard = (() => {
   board = [1,2,3,4,5,6,7,8,9]
@@ -13,18 +14,6 @@ picks = [],
 winner = false
 return { name, marker, picks};
 };
-// renderBoard = (() => {
-//   for (let i = 1; i <= 9; i++) {
-//     const div = document.createElement("div");
-//     div.className = `div${i}`;
-//     div.id = i;
-//     div.textContent = 'X'
-//     ticTacToe.appendChild(div);
-//     div.addEventListener('click', playGame)
-//   }
-// })
-
-
 
 //make the game work
 const game = (() => {
@@ -48,6 +37,13 @@ const filteredNumbers2 = board.filter(number => number !== randomNumber);
   computer.picks.push(Number(computerPlay.id))
   findOutWinner()
 
+  if (player1.winner || computer.winner) {
+    console.log('ok!!!!!')
+    document.addEventListener('mousedown', hideOverlay)
+    document.addEventListener('mousedown', hideOverlay2)
+  }
+
+
   }
   const restart = () => {
     console.log('restarting')
@@ -56,7 +52,6 @@ const filteredNumbers2 = board.filter(number => number !== randomNumber);
     computer.picks = []
     div.textContent = ''
     div.className = `div${i}`;
-
 
   }
   const div = document.createElement("div");
@@ -96,24 +91,33 @@ const filteredNumbers2 = board.filter(number => number !== randomNumber);
     console.log('player won')
     player1.winner = true;
     showOverlay()
+
+    //  restart()
   } else if (checkPlayer2 === true) {
     console.log('computer won')
     computer.winner = true;
-    showOverlay()
+    showOverlay2()
   } else if (computer.picks.length > 3) {
     console.log('draw')
+
   }
 
  }
 
- document.addEventListener('click', hideOverlay)
+
 
  function showOverlay() {
   document.getElementById("overlay").style.display = "flex";
 }
+function showOverlay2() {
+  document.getElementById("overlay2").style.display = "flex";
+}
 
 function hideOverlay() {
   document.getElementById("overlay").style.display = "none";
+}
+function hideOverlay2() {
+  document.getElementById("overlay2").style.display = "none";
 }
 
 
@@ -124,6 +128,8 @@ function hideOverlay() {
 // need to clean up
 // need to make sure that a div can't be clicked more than once
 // need logic to declare winner
+//need to make ai strong but not crucial
+// need to have option for pvp
 
 
 //players
