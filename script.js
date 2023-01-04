b15eaa888205c81e734f5d880dc1a0a40ab73329
Adams Fontil <adamsfuntil@gmail.com>
2023-01-04 15:48:46 -0500

    restart button works

--- script.js
+++ script.js
@@ -1,85 +1,132 @@
-
+hideOverlay()
 //gameboard module
 const gameBoard = (() => {
   board = [1,2,3,4,5,6,7,8,9]
-  let testing = () => console.log('testing')
   return {
-   testing,
    board
   };
 })();
 
 //players factory
 const playerFactory = (name, marker) => {
-const sayHello = () => console.log('hello!');
-picks = []
-return { name, marker, sayHello, picks };
+picks = [],
+winner = false
+return { name, marker, picks};
 };
-
-//players
-const player1 = playerFactory('player1', 'X');
-const computer = playerFactory('computer', 'O')
-const player2 = playerFactory('player2', 'O')
+// renderBoard = (() => {
+//   for (let i = 1; i <= 9; i++) {
+//     const div = document.createElement("div");
+//     div.className = `div${i}`;
+//     div.id = i;
+//     div.textContent = 'X'
+//     ticTacToe.appendChild(div);
+//     div.addEventListener('click', playGame)
+//   }
+// })
 
 
 
 //make the game work
 const game = (() => {
-
-
-
-  return {
-
-  };
-})();
-
-
 // Render tic-tac-toe board on webpage
 const ticTacToe = document.querySelector(".tic-tac-toe");
-pick1 = []
-pick2 = []
-
 for (let i = 1; i <= 9; i++) {
-  let test = () => {
+  let playGame = () => {
     div.textContent = player1.marker
-    pick1.push(div.id)
-    target = div.id
-    target = Number(target)
-    console.log(div)
-    // console.log({board})
-    // console.log({target})
+    target = Number(div.id)
     const filteredNumbers = board.filter(number => number !== target);
-  console.log(filteredNumbers);  // outputs [1, 2, 4, 5]
   board = filteredNumbers
-
-
-// Pick a random index from the array
 const index = Math.floor(Math.random() * board.length);
-
-// Get the element at the randomly chosen index
 const randomNumber = board[index];
-console.log({randomNumber})
 const filteredNumbers2 = board.filter(number => number !== randomNumber);
-  console.log(filteredNumbers2);  // outputs [1, 2, 4, 5]
   board = filteredNumbers2
   const computerPlay = document.querySelector(`.div${randomNumber}`)
   computerPlay.textContent = computer.marker
   computerPlay.className = 'selected'
   div.className = 'selected'
+  player1.picks.push(Number(div.id))
+  computer.picks.push(Number(computerPlay.id))
+  findOutWinner()
+
+  }
+  const restart = () => {
+    console.log('restarting')
+    board = [1,2,3,4,5,6,7,8,9],
+    player1.picks = []
+    computer.picks = []
+    div.textContent = ''
+    div.className = `div${i}`;
+
 
   }
   const div = document.createElement("div");
   div.className = `div${i}`;
   div.id = i;
   ticTacToe.appendChild(div);
-  div.addEventListener('click', test)
-
-
-
+  div.addEventListener('click', playGame)
+  const restartBtn = document.querySelector('.restart')
+  restartBtn.addEventListener('click', restart)
 }
+
+
+  return {
+
+
+  };
+})();
+
+
+
+//win conditions
+
+
+
+
+ findOutWinner = () => {
+  const winConditions = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1,4,7], [2,5,8], [3,6,9],  [1,5,9], [3,5,7]];
+
+  const targetArray = player1.picks
+  const targetArray2 = computer.picks
+
+
+  const checkPlayer1 = winConditions.some(array => array.every(element => targetArray.includes(element)));
+  const checkPlayer2 = winConditions.some(array => array.every(element => targetArray2.includes(element)));
+
+  if (checkPlayer1 === true) {
+    console.log('player won')
+    player1.winner = true;
+    showOverlay()
+  } else if (checkPlayer2 === true) {
+    console.log('computer won')
+    computer.winner = true;
+    showOverlay()
+  } else if (computer.picks.length > 3) {
+    console.log('draw')
+  }
+
+ }
+
+ document.addEventListener('click', hideOverlay)
+
+ function showOverlay() {
+  document.getElementById("overlay").style.display = "flex";
+}
+
+function hideOverlay() {
+  document.getElementById("overlay").style.display = "none";
+}
+
+
+
 
 
 //needs to hide as much as possible
 // need to clean up
 // need to make sure that a div can't be clicked more than once
 // need logic to declare winner
+
+
+//players
+const player1 = playerFactory('player1', 'X');
+const computer = playerFactory('computer', 'O')
+const player2 = playerFactory('player2', 'O')
