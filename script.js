4cef6b24baa4b5fcff81ce9247a1df0652affad4
Adams Fontil <adamsfuntil@gmail.com>
2023-01-09 18:42:35 -0500

    game works better

--- script.js
+++ script.js
@@ -1,77 +1,23 @@
-hideOverlay()
-hideOverlay2()
-//gameboard module
+
 const gameBoard = (() => {
   board = [1,2,3,4,5,6,7,8,9]
-  return {
-   board
-  };
 })();
 
 //players factory
 const playerFactory = (name, marker) => {
 picks = [],
 winner = false
-return { name, marker, picks};
+return {marker, picks};
 };
 
-//make the game work
-const game = (() => {
-// Render tic-tac-toe board on webpage
-const ticTacToe = document.querySelector(".tic-tac-toe");
-for (let i = 1; i <= 9; i++) {
-  let playGame = () => {
-    div.textContent = player1.marker
-    target = Number(div.id)
-    const filteredNumbers = board.filter(number => number !== target);
-  board = filteredNumbers
-const index = Math.floor(Math.random() * board.length);
-const randomNumber = board[index];
-const filteredNumbers2 = board.filter(number => number !== randomNumber);
-  board = filteredNumbers2
-  const computerPlay = document.querySelector(`.div${randomNumber}`)
-  computerPlay.textContent = computer.marker
-  computerPlay.className = 'selected'
-  div.className = 'selected'
-  player1.picks.push(Number(div.id))
-  computer.picks.push(Number(computerPlay.id))
-  findOutWinner()
+const player1 = playerFactory('player1', 'X');
+const computer = playerFactory('computer', 'O')
+const player2 = playerFactory('player2', 'O')
 
 
-doNothing = () => {
-  console.log('do nothing')
-}
-
-
-  }
-  const restart = () => {
-    console.log('restarting')
-    board = [1,2,3,4,5,6,7,8,9],
-    player1.picks = []
-    computer.picks = []
-    div.textContent = ''
-    div.className = `div${i}`;
-
-  }
-  const div = document.createElement("div");
-  div.className = `div${i}`;
-  div.id = i;
-  ticTacToe.appendChild(div);
-  div.addEventListener('click', playGame)
-  const restartBtn = document.querySelector('.restart')
-  restartBtn.addEventListener('click', restart)
-}
-
-
-  return {
-
-
-  };
-})();
-
-
-
-makeDivsBlank = () => {
+// restart game and clears board
+toMakeDivsBlank = (() => {
+  makeDivsBlank = () => {
   for (let i = 1; i <= 9; i++) {
   const divs = document.getElementById(`${i}`);
   console.log('making divs blank')
@@ -81,20 +27,62 @@ makeDivsBlank = () => {
   computer.picks = []
   divs.textContent = ''
 }
-}
+  }
+})()
+
+game = (() => {
+  const divElement = document.querySelector('.tic-tac-toe');
+
+divElement.addEventListener('click', function(event) {
+  const divClass = event.target.className;
+  console.log(divClass)
+  playGame = (() => {
+  div = document.querySelector(`.${divClass}`)
+div.textContent = player1.marker
+  target = Number(div.id)
+  const filteredNumbers = board.filter(number => number !== target);
+  board = filteredNumbers
+  const index = Math.floor(Math.random() * board.length);
+  const randomNumber = board[index];
+  const filteredNumbers2 = board.filter(number => number !== randomNumber);
+  board = filteredNumbers2
+  const computerPlay = document.querySelector(`.div${randomNumber}`)
+  computerPlay.textContent = computer.marker
+  computerPlay.className = 'selected'
+  div.className = 'selected'
+  player1.picks.push(Number(div.id))
+  computer.picks.push(Number(computerPlay.id))
+  findwinner()
+  })()
+});
+})()
+
+
+createBoard = (() => {
+  const ticTacToe = document.querySelector(".tic-tac-toe");
+  for (let i = 1; i <= 9; i++) {
+    const div = document.createElement("div");
+    div.className = `div${i}`;
+    div.id = i;
+    ticTacToe.appendChild(div);
+  }
+    const restartBtn = document.querySelector('.restart')
+    restartBtn.addEventListener('click', makeDivsBlank)
+
+  })()
+
 
 //win conditions
 
 
 
 
- findOutWinner = () => {
+ findOutWinner = (() => {
+  findwinner = () => {
   const winConditions = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1,4,7], [2,5,8], [3,6,9],  [1,5,9], [3,5,7]];
 
   const targetArray = player1.picks
   const targetArray2 = computer.picks
-
-
   const checkPlayer1 = winConditions.some(array => array.every(element => targetArray.includes(element)));
   const checkPlayer2 = winConditions.some(array => array.every(element => targetArray2.includes(element)));
 
@@ -102,8 +90,6 @@ makeDivsBlank = () => {
     console.log('player won')
     player1.winner = true;
     showOverlay()
-
-    //  restart()
   } else if (checkPlayer2 === true) {
     console.log('computer won')
     computer.winner = true;
@@ -112,22 +98,19 @@ makeDivsBlank = () => {
     console.log('draw')
   }
   if (player1.winner || computer.winner) {
-    player1.winner = false;
-    computer.winner = false;
     console.log({player1})
     document.addEventListener('mousedown', hideOverlay)
     document.addEventListener('mousedown', hideOverlay2)
     document.addEventListener('mousedown', makeDivsBlank)
-
+    player1.winner = false;
+    computer.winner = false;
 
   } else if (player1.winner === false) {
-    console.log('testing')
+    console.log('remove event listener')
     document.removeEventListener('mousedown', makeDivsBlank)
-    document.addEventListener('mousedown', doNothing)
-
   }
-
- }
+}
+ })()
 
 
 
@@ -149,15 +132,10 @@ function hideOverlay2() {
 
 
 
-//needs to hide as much as possible
-// need to clean up
-// need to make sure that a div can't be clicked more than once
-// need logic to declare winner
-//need to make ai strong but not crucial
+//needs to hide as much as possible -- Worked on
+// need to clean up -- Working on
+// need to make sure that a div can't be clicked more than once --Done
+// need logic to declare winner -- Done
+//need to make ai strong but not that crucial
 // need to have option for pvp
-
-
 //players
-const player1 = playerFactory('player1', 'X');
-const computer = playerFactory('computer', 'O')
-const player2 = playerFactory('player2', 'O')
